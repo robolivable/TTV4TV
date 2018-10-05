c11eaa2a3455ac51e8f53b3123dd4067577df817
Robert Oliveira <oliveira.rlde@gmail.com>
2018-10-05 01:45:12 -0400

    Add key press event hook to listen to back presses

--- src/components/ttv4tv.jsx
+++ src/components/ttv4tv.jsx
@@ -16,7 +16,7 @@ export default class TTV4TV extends React.Component {
     this._handleMediaClick = this._handleMediaClick.bind(this)
     this._handleListOnFocus = this._handleListOnFocus.bind(this)
     this._handleVerticalListOnBlur = this._handleVerticalListOnBlur.bind(this)
-    this._handleTwitchPlayerOnClose = this._handleTwitchPlayerOnClose.bind(this)
+    this._handleOnKeyDown = this._handleOnKeyDown.bind(this)
     this.state = {
       activeFocus: null,
       isMediaPlayerEnabled: false,
@@ -35,6 +35,8 @@ export default class TTV4TV extends React.Component {
     if (this.state.fetched) {
       return
     }
+
+    document.addEventListener("keydown", this._handleOnKeyDown, false)
 
     const lists = await Promise.all([
       // async () => ({ name: 'subscribed', val: await this.twitch.subscribed()}),
@@ -148,7 +150,14 @@ export default class TTV4TV extends React.Component {
     this.setState({ activeFocus: null })
   }
 
-  _handleTwitchPlayerOnClose () {
-    this.setState({ isMediaPlayerEnabled: false, media: null })
+  _handleOnKeyDown (e) {
+    switch (e.keyCode) {
+      case (0x1B):
+      case (0x1CD):
+        e.preventDefault()
+        this.setState({ isMediaPlayerEnabled: false, media: null })
+      default:
+        break
+    }
   }
 }
