14f7f00509642765189ab9b9a4e464aea0cfb7b3
Robert Oliveira <robert.oliveira@adp.com>
2018-10-05 20:37:38 -0400

    Implement search bar UI

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
 
+import { Focusable } from 'react-key-navigation'
+
 export default class SearchBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this._handleSearchOnBlur = this._handleSearchOnBlur.bind(this)
+    this._handleSearchOnEnterDown = this._handleSearchOnEnterDown.bind(this)
+    this._handleSearchOnFocus = this._handleSearchOnFocus.bind(this)
+
+    this.state = {
+      activeFocus: false
+    }
+  }
+
   render () {
-    return null // TODO: implement search bar
+    return (
+      <Focusable
+        onFocus={this._handleSearchOnFocus}
+        onBlur={this._handleSearchOnBlur}
+        onEnterDown={this._handleSearchOnEnterDown}
+        navDefault
+      >
+        <div
+          className={this.state.activeFocus ?
+            'search-box-placeholder-focus'
+          : ''}
+          id='search-box-placeholder'
+        >
+          <i className='fa fa-search' />
+        </div>
+      </Focusable>
+    )    
+  }
+
+  _handleSearchOnBlur () {
+    this.setState({ activeFocus: false })
+  }
+
+  _handleSearchOnEnterDown (e, navigation) {
+    console.log(e, navigation)
+  }
+
+  _handleSearchOnFocus () {
+    this.setState({ activeFocus: true })
   }
 }
