0f29e7f160787f92844a52b06fd27e897f04b6fe
Robert Oliveira <robert.oliveira@adp.com>
2018-10-10 22:18:34 -0400

    Implement search bar value state

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
 import { Focusable } from 'react-key-navigation'
 
+const SESSION_SEARCH_BAR_INPUT_VALUE = 'search-bar-input-value'
+
 export default class SearchBar extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor (...args) {
+    super(...args)
     this._handleSearchOnBlur = this._handleSearchOnBlur.bind(this)
     this._handleSearchOnEnterDown = this._handleSearchOnEnterDown.bind(this)
     this._handleSearchOnFocus = this._handleSearchOnFocus.bind(this)
+    this.searchBarInputRef = null
 
     this.state = {
       activeFocus: false
@@ -22,16 +24,26 @@ export default class SearchBar extends React.Component {
         onEnterDown={this._handleSearchOnEnterDown}
         navDefault
       >
-        <div
-          className={this.state.activeFocus ?
-            'search-box-placeholder-focus'
-          : ''}
-          id='search-box-placeholder'
-        >
+        <div className={
+          `search-box-placeholder${this.state.activeFocus ? ' focus' : ''}`
+        }>
           <i className='fa fa-search' />
+          <input
+            id='search-bar-input'
+            ref={element => { this.searchBarInputRef = element }}
+            type='text'
+            value={this.state.searchValue}
+            style={this.state.activeFocus ? { backgroundColor: 'white' } : {}}
+            onKeyDown={`((e) => {
+              sessionStorage.setItem(
+                '${SESSION_SEARCH_BAR_INPUT_VALUE}',
+                e.target.value
+              )
+            })(event)`}
+          />
         </div>
       </Focusable>
-    )    
+    )
   }
 
   _handleSearchOnBlur () {
@@ -39,7 +51,7 @@ export default class SearchBar extends React.Component {
   }
 
   _handleSearchOnEnterDown (e, navigation) {
-    console.log(e, navigation)
+    this.searchBarInputRef.focus()
   }
 
   _handleSearchOnFocus () {
